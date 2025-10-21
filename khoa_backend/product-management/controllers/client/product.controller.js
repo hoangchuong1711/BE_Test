4aa7d5b5cf9fffc1925ffd17ab8804a1c4daac66
hoangchuong <nguyenlehoangchuong1711@gmail.com>
2025-10-21 20:42:07 +0700

    update san pham

--- khoa_backend/product-management/controllers/client/product.controller.js
+++ khoa_backend/product-management/controllers/client/product.controller.js
@@ -1,5 +1,19 @@
-module.exports.index = (req, res) => {
+const Product = require("../../models/products.model");
+
+module.exports.index = async (req, res) => {
+    const product = await Product.find({
+        status: "active",
+        deleted: false
+    });
+
+    const newProduct = product.map(item => {
+        item.newPrice= item.price-(item.price*item.discountPercentage/100);
+        return item;
+    })
+
+    console.log(newProduct);
     res.render("client/pages/products/index",{
-        pageTitle:"San Pham"
+        pageTitle:"San Pham",
+        product: newProduct
     });
 };
