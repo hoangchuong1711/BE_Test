7b51c6254bf0c7f63ca1ff9f2affc19cc9578b0d
hoangchuong <nguyenlehoangchuong1711@gmail.com>
2025-10-22 13:42:25 +0700

    update admin page

---
+++ khoa_backend/product-management/routes/admin/index.route.js
@@ -0,0 +1,7 @@
+const dashboarRoutes = require("./dashboar.route");
+const systemConfig = require("../../config/system");
+
+module.exports = (app) => {
+  const PATH_ADMIN = systemConfig.prefixAdmin;
+  app.use(PATH_ADMIN + "/dashboar", dashboarRoutes);
+};
