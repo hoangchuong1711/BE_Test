eef2b8ee5fb98b4317f9ffcb4a34b5bd29c0252d
hoangchuong <nguyenlehoangchuong1711@gmail.com>
2025-10-22 21:18:38 +0700

    thêm tính năng lọc trạng thái

---
+++ khoa_backend/product-management/public/admin/js/script.js
@@ -0,0 +1,18 @@
+//button status
+const buttonStatus= document.querySelectorAll("[button-status]");
+if(buttonStatus.length>0){
+    buttonStatus.forEach(button=>{
+        let url = new URL(window.location.href); 
+
+        button.addEventListener("click",()=>{
+            const status = button.getAttribute("button-status");
+            if(status){
+                url.searchParams.set("status", status);
+            }else{
+                url.searchParams.delete("status");
+            }
+
+            window.location.href= url.href;
+        })
+    })
+}
