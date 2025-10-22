b35c2db8d1e05aa87b649a32887a4cf6705e40df
hoangchuong <nguyenlehoangchuong1711@gmail.com>
2025-10-22 20:06:47 +0700

    lấy sản phẩm từ db in ra màn hình

--- khoa_backend/product-management/controllers/admin/product.controller.js
+++ khoa_backend/product-management/controllers/admin/product.controller.js
@@ -1,6 +1,13 @@
+const product = require("../../models/products.model");
+
 // [GET] /admin/products
-module.exports.index = (req, res) => {
-    res.render("admin/pages/products/index", {
-        pageTitle:"Trang tong quan"
+module.exports.index = async (req, res) => {
+    const products = await product.find({
+        deleted: false,
     });
-}
+    console.log(products);
+    res.render("admin/pages/products/index", {
+        pageTitle: "Trang tong quan",
+        products: products
+    });
+};
