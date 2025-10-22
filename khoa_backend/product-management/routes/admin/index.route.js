ca7408e1fe712b54ad5d5d295b877237aa448f8f
hoangchuong <nguyenlehoangchuong1711@gmail.com>
2025-10-22 15:03:01 +0700

    update trang san pham

--- khoa_backend/product-management/routes/admin/index.route.js
+++ khoa_backend/product-management/routes/admin/index.route.js
@@ -1,7 +1,11 @@
 const dashboarRoutes = require("./dashboar.route");
+const productRoutes = require("./product.route");
+
 const systemConfig = require("../../config/system");
 
 module.exports = (app) => {
   const PATH_ADMIN = systemConfig.prefixAdmin;
   app.use(PATH_ADMIN + "/dashboard", dashboarRoutes);
+
+  app.use(PATH_ADMIN + "/products", productRoutes);
 };
