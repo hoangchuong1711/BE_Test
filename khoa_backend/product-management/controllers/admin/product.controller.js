9f051cfb42e7a0287fff62276be3bc3d98bac6c8
hoangchuong <nguyenlehoangchuong1711@gmail.com>
2025-10-23 15:34:56 +0700

    tinh năng phân trang

--- khoa_backend/product-management/controllers/admin/product.controller.js
+++ khoa_backend/product-management/controllers/admin/product.controller.js
@@ -1,6 +1,7 @@
 const product = require("../../models/products.model");
 const filterStatusHelper = require("../../helpers/filterStatus");
 const searchHelper = require("../../helpers/search");
+const Product = require("../../models/products.model");
 
 // [GET] /admin/products
 module.exports.index = async (req, res) => {
@@ -20,12 +21,30 @@ module.exports.index = async (req, res) => {
         find.title= objectSearch.regex;
     }
 
-    const products = await product.find(find);
+    //pagination
+    let objectPagination={
+        currentPage:1,
+        limitItem:4
+    }
+
+    if(req.query.page){
+        objectPagination.currentPage=parseInt(req.query.page);
+    }
+
+    objectPagination.skip=(objectPagination.currentPage-1) * objectPagination.limitItem
+
+    const countProduct = await Product.countDocuments(find);
+    const totalPage=Math.ceil(countProduct/objectPagination.limitItem);
+    const products = await product.find(find).limit(objectPagination.limitItem).skip(objectPagination.skip);
+    objectPagination.totalPage=totalPage;
+    
+    
     //console.log(products);
     res.render("admin/pages/products/index", {
         pageTitle: "Trang tong quan",
         products: products,
         filterStatus: filterStatus,
-        keyword: objectSearch.keyword
+        keyword: objectSearch.keyword,
+        pagination: objectPagination
     });
 };
--- khoa_backend/product-management/public/admin/js/script.js
+++ khoa_backend/product-management/public/admin/js/script.js
@@ -32,3 +32,17 @@ if(fromSearch){
         window.location.href= url.href;
     });
 }
+
+//pagination
+const buttonsPagination= document.querySelectorAll("[button-pagination]")
+if(buttonsPagination){
+    let url = new URL(window.location.href); 
+
+    buttonsPagination.forEach(button=>{
+        button.addEventListener("click",()=>{
+            const page = button.getAttribute("button-pagination")
+            url.searchParams.set("page", page);
+            window.location.href= url.href;
+        })
+    })
+}
